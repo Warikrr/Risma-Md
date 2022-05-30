ec108bf9bf287841077c2cdb431ae87e54003ed7
Mursid S <97183746+Botwa021@users.noreply.github.com>
2022-05-30 22:19:33 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,14 +1,14 @@
 let handler = async (m, { conn, usedPrefix }) => conn.sendButton(m.chat, `
-╭─「 Donasi • Dana 」
-│ • Telkomsel [081320170984]
-│ • Dana  [081320170984]
-╰────
-╭─「 *NOTE* 」
-│ > Ingin donasi? Wa.me/6281320170984
+┏──「 Donasi • Dana 」─⬣
+│ • Gopay : [088233832771]
+│ • Dana : [088233832771]
+╰────────⬣
+┏──「 *NOTE* 」─⬣
+│ > Ingin donasi? wa.me/6288233832771
 │ _Hasil donasi akan digunakan buat sewa_
 │ _atau beli *RDP/VPS* agar bot bisa jalan_
 │ _24jam tanpa kendala_
-╰────
+┗────────⬣
 `.trim(), wm, 'Menu', usedPrefix + 'menu', m) // Tambah sendiri kalo mau
 handler.help = ['donasi']
 handler.tags = ['about']
