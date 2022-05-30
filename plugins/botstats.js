c9ad8692d2abcfb9455bf1cf1ac79ebe3808d1cd
Mursid S <97183746+Botwa021@users.noreply.github.com>
2022-05-30 22:53:53 +0700

    Update botstats.js

--- plugins/botstats.js
+++ plugins/botstats.js
@@ -10,25 +10,25 @@ let handler = async (m, { conn }) => {
 
 
     m.reply(`
-┌─〔 Status 〕
-├ Aktif selama ${uptime}
-├ *${groups.length}* Grup
-├ *${chats.length - groups.length}* Chat Pribadi
-├ *${Object.keys(global.db.data.users).length}* Pengguna
-├ ${block == undefined ? '*0* Diblokir' : '*' + block.length + '* Diblokir'}
-├ *${Object.entries(global.db.data.chats).filter(chat => chat[1].isBanned).length}* Chat Terbanned
-├ *${Object.entries(global.db.data.users).filter(user => user[1].banned).length}* Pengguna Terbanned
-└────
+┏──『 Status 』──⬣
+│⬡ Aktif selama ${uptime}
+│⬡ *${groups.length}* Grup
+│⬡ *${chats.length - groups.length}* Chat Pribadi
+│⬡ *${Object.keys(global.db.data.users).length}* Pengguna
+│⬡ ${block == undefined ? '*0* Diblokir' : '*' + block.length + '* Diblokir'}
+│⬡ *${Object.entries(global.db.data.chats).filter(chat => chat[1].isBanned).length}* Chat Terbanned
+│⬡ *${Object.entries(global.db.data.users).filter(user => user[1].banned).length}* Pengguna Terbanned
+┗────────⬣
 
-┌─〔 Pengaturan 〕
-├ ${anon ? '✅' : '❌'} *Anon Chat*
-├ ${anticall ? '✅' : '❌'} *Anti Call*
-├ ${backup ? '✅' : '❌'} *Auto Backup DB*
-├ ${groupOnly ? '✅' : '❌'} *Mode Grup*
-├ ${jadibot ? '✅' : '❌'} *Jadi Bot*
-├ ${tag ? '✅' : '❌'} *Anti Tag Owner*
-├ ${self ? '✅' : '❌'} *Mode Self*
-└────
+┏──『 Pengaturan 』──⬣
+│⬡ ${anon ? '✅' : '❌'} *Anon Chat*
+│⬡ ${anticall ? '✅' : '❌'} *Anti Call*
+│⬡ ${backup ? '✅' : '❌'} *Auto Backup DB*
+│⬡ ${groupOnly ? '✅' : '❌'} *Mode Grup*
+│⬡ ${jadibot ? '✅' : '❌'} *Jadi Bot*
+│⬡ ${tag ? '✅' : '❌'} *Anti Tag Owner*
+│⬡ ${self ? '✅' : '❌'} *Mode Self*
+┗────────⬣
     `.trim())
 }
 handler.help = ['stats']
