533e541062b9a79ac8ea23ef381de641b0e298c5
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-07-13 23:20:14 +0700

    Update broadcast-image.js

--- plugins/broadcast-image.js
+++ plugins/broadcast-image.js
@@ -3,7 +3,7 @@ let handler  = async (m, { conn, text }) => {
   let chats = Object.keys(await conn.chats)
   conn.reply(m.chat, `_Mengirim pesan broadcast ke ${chats.length} chat_`, m)
   for (let id of chats) {
-       let bcbg = 'https://telegra.ph/file/b2ae8a0e437e4252d5124.png'
+       let bcbg = 'https://telegra.ph/file/c89d0fafc773483286e13.png'
        await conn.delay(1500)
        await conn.send2ButtonImg(id, bcbg, text.trim(), wm, 'Menu', '.menu', 'Owner', '.owner', ftroli)
      }
