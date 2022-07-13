397c508b5a5fd46dcdedfb325fdb116815d816a8
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-07-13 18:47:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -41,7 +41,7 @@ global.urlnya = "https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ"
 
 //============= callButtons =============//
 global.dtc = '𝒄𝒂𝒍𝒍 𝒐𝒘𝒏𝒆𝒓'
-global.phn = '+62 882-3383-2771'
+global.phn = '+62 858-5053-9404'
 
 //============= Games ================//
 global.benar = '_*Benar*_'
