72d8bf141d01181ed050e7cbd0b715b1ff95355b
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-07-14 13:23:44 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -32,9 +32,9 @@ module.exports = {
         global.author = global.data.owner
         //global.author = '𝐉𝐚𝐫𝐨𝐭'
         global.wm2 = global.data.namabot + ' ' + global.data.owner
-        global.wm = '© 𝐑𝐢𝐬𝐦𝐚 𝐁𝐨𝐭'
+        global.wm = '© 𝐀𝐥𝐩𝐢 𝐁𝐨𝐭'
         global.colong1 = 'Ciss 📸'
-        global.colong2 = '𝐑𝐢𝐬𝐦𝐚 𝑩𝒐𝒕'
+        global.colong2 = '𝐀𝐥𝐩𝐢 𝑩𝒐𝒕'
         global.kontak2 = [
         ['6285850539404', '𝐉𝐚𝐫𝐨𝐭', '𝑫𝒆𝒗𝒆𝒍𝒐𝒑𝒆𝒓 𝑩𝒐𝒕', 'https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ', true],
         ['6285713137903', await this.getName('6285713137903@s.whatsapp.net'), '𝑷𝒂𝒓𝒕𝒏𝒆𝒓 𝐀𝐥𝐩𝐢 𝑩𝒐𝒕', 'https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ', true], 
