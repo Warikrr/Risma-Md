6fd0c9c06ae2933825a428fcc9e843f0773dc2cd
revkykira <100487320+revkykira@users.noreply.github.com>
2022-08-09 12:01:20 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,10 +1,10 @@
 let handler = async (m, { conn, usedPrefix }) => conn.sendButton(m.chat, `
 ┏──「 Donasi • Dana 」─⬣
-│ • Pulsa : 085850539404
-│ • Dana : 085850539404
+│ • Pulsa : 081802857821
+│ • Dana : 081802857821
 ╰────────⬣
 ┏──「 *NOTE* 」─⬣
-│ > Ingin donasi? wa.me/6285850539404
+│ > Ingin donasi? wa.me/6285881281762
 │ _Hasil donasi akan digunakan buat sewa_
 │ _atau beli *RDP/VPS* agar bot bisa jalan_
 │ _24jam tanpa kendala_
