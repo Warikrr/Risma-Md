1ccf18bb700b86d07124ebd4a0e551f86675b53b
revkykira <100487320+revkykira@users.noreply.github.com>
2022-08-11 09:06:19 +0700

    Update neko.js

--- plugins/neko.js
+++ plugins/neko.js
@@ -6,7 +6,7 @@ let handler = async(m, { conn }) => {
   if (!res.ok) throw await res.text()
   let json = await res.json()
   if (!json.url) throw 'Error!'
-  conn.sendFile(m.chat, json.url, '', 'Nyaa', m)
+  conn.sendFile(m.chat, json.url, '', 'pengen coli ya? *_sender by RevkyChans UwU_*', m)
 }
 handler.help = ['neko']
 handler.tags = ['internet']
