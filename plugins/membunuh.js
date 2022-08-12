2ec921ac55af9f7177759b7c45f63f7c1e6478ed
revkykira <100487320+revkykira@users.noreply.github.com>
2022-08-12 13:00:13 +0700

    Update membunuh.js

--- plugins/membunuh.js
+++ plugins/membunuh.js
@@ -7,26 +7,17 @@ let nomors = m.sender
   let who
   if (m.isGroup) who = m.mentionedJid[0]
   else who = m.chat
-  if (!who) throw 'Tag salah satu lah'
-  if (typeof db.data.users[who] == 'undefined') throw 'Pengguna tidak ada didalam data base'
   let __timers = (new Date - global.db.data.users[m.sender].lastbunuhi)
-  let _timers = (3600000 - __timers) 
+  let _timers = (36000 - __timers) 
   let timers = clockString(_timers)
   let users = global.db.data.users
   if (new Date - global.db.data.users[m.sender].lastbunuhi > 3600000){
-   if (10 > users[who].health) throw 'Target sudah tidak memiliki health'
-   if (100 > users[who].money) throw 'Target tidak memiliki apapun :('
-  users[who].health -= healtu * 1
-  users[who].money -= dapat * 1
-  users[m.sender].money += dapat * 1
-  global.db.data.users[m.sender].lastbunuhi = new Date * 1
-  m.reply(`Target berhasil di bunuh dan kamu mengambil money target sebesar\n${dapat} Money\nDarah target berkurang -${healtu} Healt`)
-}else conn.reply(m.chat, `Anda sudah membunuh orang dan berhasil sembunyi , tunggu ${timers} untuk membunuhnya lagi`, m)
+}else conn.reply(m.chat, `https://ponselharian.com/ojPGgnc9TT49`, m)
 }
 
-handler.help = ['membunuh *@tag*']
+handler.help = ['link mc']
 handler.tags = ['rpg']
-handler.command = /^membunuh$/
+handler.command = /^link mc$/
 handler.limit = true
 handler.group = true
 
@@ -36,7 +27,7 @@ function pickRandom(list) {
     return list[Math.floor(Math.random() * list.length)]
 }
 function clockString(ms) {
-  let h = Math.floor(ms / 3600000)
+  let h = Math.floor(ms / 36000)
   let m = Math.floor(ms / 60000) % 60
   let s = Math.floor(ms / 1000) % 60
   console.log({ms,h,m,s})
